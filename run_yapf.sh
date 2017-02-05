7c42d63d720412a7bfbd60f19b3133020c339969
PLD <pierre-louis@dourneau.me>
2017-02-05 15:20:53 +0100

    Add yapf style file and autolinting bash script
    When run, `run_yapf.sh` calls yapf a first time to show what changes can
    be made to the sources files to match closer to the PEP-8 and a second
    time to apply these syntax fixes.
    
    It will enable us to have a more consistent coding style, just by
    running this script before committing.

---
+++ run_yapf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+RESET="\e[0m"
+BOLD="\e[1m"
+RED="\e[91m"
+GREEN="\e[92m"
+YELLOW="\e[93m"
+
+yapf -rdp src/ test/
+echo -en "${YELLOW}Apply these modifications? [Yy/*]${RESET} "
+read -n 1 choice
+echo
+if [ $choice = "y" -o $choice = "Y" ]; then
+  echo -n "Applying..."
+  yapf -rip src/ test/
+  if [ $? -eq 0 ]; then
+    echo -e " ${BOLD}${GREEN}Done ✓${RESET}"
+  else
+    echo -e " ${BOLD}${RED}Failed ✗${RESET}"
+  fi
+else
+  echo -e "${RED}Aborting${RESET}..."
+  exit 0
+fi
