0efaa5a73124618ab9108bc7ca84d60c28146e10
PLD <pierre-louis@dourneau.me>
2017-02-06 16:20:56 +0100

    Add isort script
    isort is a little utility that automatically sort imports in python
    files. It comes to complement yapf on having a consistent coding style.
    run_isort.sh is essentially the same script as run_yapf except it runs
    isort instead of yapf.
    isort is also now in the requirements.txt file.

---
+++ run_isort.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+RESET="\e[0m"
+BOLD="\e[1m"
+RED="\e[91m"
+GREEN="\e[92m"
+YELLOW="\e[93m"
+
+isort --recursive --diff src/ test/
+echo -en "${YELLOW}Apply these modifications? [Yy/*]${RESET} "
+read -n 1 choice
+echo
+if [ $choice = "y" -o $choice = "Y" ]; then
+  echo -n "Applying..."
+  isort --recursive --apply src/ test/
+  if [ $? -eq 0 ]; then
+    echo -e " ${BOLD}${GREEN}Done ✓${RESET}"
+  else
+    echo -e " ${BOLD}${RED}Failed ✗${RESET}"
+  fi
+else
+  echo -e "${RED}Aborting${RESET}..."
+fi
