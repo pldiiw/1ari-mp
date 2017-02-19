cc5121726a6b1996f428010c47a65a02dfb2229b
PLD <pierre-louis@dourneau.me>
2017-02-19 22:32:12 +0100

    Add delimitation lines for better readability
    This adds the two lines per sidebar annotation that help read what the
    '< CLEAR' annotation points to.

--- src/component/sidebar_annotation.py
+++ src/component/sidebar_annotation.py
@@ -1,6 +1,10 @@
 """Functions and procedures used to draw with sidebar annotations."""
 
+import pygame
+
 from component.write_text import write_left_aligned_text
+
+WHITE = (255, 255, 255)
 
 
 def draw_sidebar_annotation(text: str, column_number: int, window) -> None:
@@ -16,3 +20,12 @@ def draw_sidebar_annotation(text: str, column_number: int, window) -> None:
     annotation_surface = window.subsurface(annotation_pos,
                                            annotation_dimensions)
     write_left_aligned_text(text, annotation_surface)
+
+    # Add the little delimitation lines
+    startpos = (window.get_width() / 10 * 9 / 100 * 5, annotation_pos[1])
+    endpos = (window.get_width() / 10 * 9 / 100 * 95, annotation_pos[1])
+    pygame.draw.aaline(window, WHITE, startpos, endpos)
+
+    startpos = (startpos[0], startpos[1] + annotation_dimensions[1])
+    endpos = (endpos[0], endpos[1] + annotation_dimensions[1])
+    pygame.draw.aaline(window, WHITE, startpos, endpos)
